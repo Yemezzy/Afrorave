f1e26d3070d5c870ed91c155b32518f278891b70
Yemezzy <ayubayelmee222@gmail.com>
2024-09-14 17:30:58 +0100

    first commit

---
+++ src/Pages/Afrorave.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import { FaInstagram } from 'react-icons/fa'
+import { FaXTwitter } from 'react-icons/fa6'
+import { LuFacebook } from 'react-icons/lu'
+
+const Afrorave = () => {
+  return (
+    <div className='bmm'>
+        <nav className='md:py-3 py-5 px-10  flex items-center justify-between text-xl font-bold '>
+       <img src="/image/RAVE.svg" alt="image" className='md:w-[5%] w-20 md:mt-5'/> 
+       <div>
+        <marquee behavior="" direction="left">
+        <p>Ravers get ready</p>   
+        </marquee>
+        </div> 
+        </nav>
+
+        <div className='md:px-10 px-3 mt-5'>
+            <div className='border- rounded-xl  h-[500px] border-black lol p-5'>
+    {/* <div className='flex justify-center'>
+    <div className='grid grid-cols-3 gap-10'>
+            <section className=' w-[370px] h-[300px] pp1 border- border-black'>
+           
+            </section>
+            <section className='h-[300px] pp2 w-[370px] border- border-black'>
+           
+            </section>
+            <section className='h-[300px] pp3 w-[370px] border- border-black'>
+
+            </section>
+        </div>
+    </div>
+    <div className='flex justify-center'>
+    <div className='grid grid-cols-2 gap-10 mt-2'>
+            <section className=' w-[370px] h-[300px] pp4 border- border-black'>
+           
+            </section>
+            <section className='h-[300px] pp5 w-[370px] border- border-black'>
+           
+            </section>
+        </div>
+    </div> */}
+            </div>
+
+            <div className='md:mt-10 mt-5'>
+<p className='md:text-lg text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium adipisci aspernatur voluptas consectetur omnis nihil pariatur esse, 
+    repellat, nisi possimus eos officia, officiis sint deleniti. Magni non voluptatum perferendis mollitia. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+     Reprehenderit fugit dolores tempora impedit repellat aliquam quaerat iusto itaque asperiores nesciunt.</p>
+            </div>
+
+
+            <div className='mt-10 md:flex items-center justify-between rounded-2xl bg-[#DB0056] p-5 md:h-[150px] '>
+<div>
+<p className='font-bold md:text-xl'>Are you ready RAVERS?</p>
+<p className='font-bold md:text-3xl text-xl'>Subscribe to our newsletter</p>
+</div>
+<div className=' md:w-[50%] mt-3 md:mt-0'>
+<p className='font-bold md:text-lg text-right '>Enter your email address here</p>
+<div className='flex'>
+<input type="text" className='outline-none px-2 rounded-sm h-[50px] w-full'/>
+<button className='h-[50px] bg-black text-white p-3 md:text-base text-sm'>Subscribe</button>
+</div>
+</div>
+            </div>
+
+            <div className='flex gap-10 md:py-10 py-5 text-2xl justify-center text-[#DB0056] uppercase font-semibold'>
+    <FaInstagram/>
+    <FaXTwitter />
+    <LuFacebook />
+   
+</div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Afrorave
---
+++ src/Pages/Fontpage.js
@@ -0,0 +1,145 @@
+import React from 'react'
+import { FaInstagram } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { LuFacebook } from "react-icons/lu";
+
+const Fontpage = () => {
+  return (
+    <div className='mx-10 bmm'>
+        <div className='flex justify-center border-b-2 border-black py-5 mx-10 mr-[4.2%]'>
+       <img src="/image/RAVE.svg" alt="image" className='w-[7%] mt-5'/>  
+        </div>
+<div className='flex justify-between'>
+<section>
+<div>
+    <div className='border-l-2 ml-10 border-black h-[50px]'>
+
+    </div>
+   <section className='border-2 order-[#DB0056] border-black w-[100px] h-[100px]'>
+<img src="/image/afro10.png" alt="" />
+</section>
+   </div>
+   <div>
+    <div className='border-l-2 ml-10 border-black h-[50px]'>
+
+    </div>
+    <section className='border-2 order-[#DB0056] border-black w-[100px] h-[100px]'>
+<img src="/image/afro10.png" alt="" />
+</section>
+
+<div className='border-l-2 ml-10 border-black h-[50px]'>
+
+</div>
+   </div>
+</section>
+
+
+<section className='p-10 text-lg pt-2'>
+<ul className='flex gap-10 justify-center uppercase font-semibold'>
+    <li className=' border-b-4 border-[#DB0056]'>Home</li>
+    <li className=' border-b-4 border-[#DB0056]'>About Us</li>
+    <li className=' border-b-4 border-[#DB0056]'>Gallery</li>
+    <li className=' border-b-4 border-[#DB0056]'>Contact</li>
+ 
+</ul>
+
+    <p className='mt-16'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum reprehenderit, 
+        laudantium ipsa, veniam amet sit placeat modi voluptatem possimus sunt delectus nostrum harum id a? 
+        Quisquam maiores repellendus blanditiis alias possimus! Odit aliquid qui iure culpa exercitationem consequuntur nostrum! 
+        Incidunt error eligendi odit consectetur dolorem laudantium. Similique, explicabo tempore. Laudantium?
+        Incidunt error eligendi odit consectetur dolorem laudantium. Similique, explicabo tempore. Laudantium?
+        Incidunt error eligendi odit consectetur dolorem laudantium. Similique, explicabo tempore. Laudantium?
+    </p>
+
+    <div className='flex gap-10 pt-10 text-2xl justify-center text-[#DB0056] uppercase font-semibold'>
+    <FaInstagram />
+    <FaXTwitter />
+    <LuFacebook />
+   
+</div>
+
+</section>
+
+
+<section>
+<div>
+    <div className='border-l-2 ml-10 border-black h-[50px]'>
+
+    </div>
+    <section className='border-2 order-[#DB0056] border-black w-[100px] h-[100px]'>
+<img src="/image/afro10.png" alt="" />
+</section>
+   </div>
+   <div>
+    <div className='border-l-2 ml-10 border-black h-[50px]'>
+
+    </div>
+    <section className='border-2 order-[#DB0056] border-black w-[100px] h-[100px]'>
+<img src="/image/afro10.png" alt="" />
+</section>
+   </div>
+   <div className='border-l-2 ml-10 border-black h-[50px]'>
+
+</div>
+</section>
+</div>
+<div className='flex justify-center border-b-2 border-black mx-10 mr-[4.2%]'>
+
+</div>
+
+
+    <div className='mt-20'>
+<div className='pt-10'>
+    <p className='font-bold text-4xl'>
+        ABOUT US
+    </p>
+    <div className='grid grid-cols-2 items-center'>
+        <section>
+            <p className='mt-10 text-lg'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nam pariatur quisquam accusamus,
+                 ex dicta autem sit incidunt cumque odio fuga, ullam voluptate aperiam velit reprehenderit deserunt quia 
+                 doloribus veniam aliquam, nostrum impedit eligendi. 
+                Ab placeat iure fugiat perspiciatis est ad ipsum!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nam pariatur quisquam accusamus,
+                 ex dicta autem sit incidunt cumque odio fuga, ullam voluptate aperiam velit reprehenderit deserunt quia 
+                 doloribus veniam aliquam, nostrum impedit eligendi. 
+                Ab placeat iure fugiat perspiciatis est ad ipsum!
+            </p>
+        </section>
+        <section className='flex justify-center'>
+            <img src="/image/IMG_8846.jpeg" alt="" className='w-[70%]'/>
+        </section>
+    </div>
+
+<div className='mt-20'>
+<section className='border-2 h-[150px] border-[#DB0056] rounded-xl'>
+
+</section>
+</div>
+ 
+<div className='mt-20 '>
+<p className='font-bold text-4xl'>
+GALLERY
+    </p>
+<div className='mt-5 grid grid-cols-3 gap-5'>
+<section className='h-[300px] hover:h-[320px] transition-all hover:w-[100%] w-[95%] border-2 border-black'></section>
+<section className='h-[320px] hover:h-[330px] transition-all hover:w-[100%] w-[95%] border-2 border-black'></section>
+<section className='h-[300px] hover:h-[320px] transition-all hover:w-[100%] w-[95%] border-2 border-black'></section>
+</div>
+<a href="">
+<p className='text-center mt-5 text-xl underline text-[#DB0056]'>View More</p>
+</a>
+</div>
+
+<div className='mt-20'>
+
+</div>
+
+</div>
+    </div>
+    </div>
+  )
+}
+
+export default Fontpage
